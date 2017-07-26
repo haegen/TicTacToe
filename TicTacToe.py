3c9fe8dd5454dddc4fc84f2277923ef11af5d3dc
Hagen Timper <hagen.timper@gmx.de>
2017-07-26 20:22:36 +0200

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+# Python 2.7
 
 LEER = " "
 X = 'X'
@@ -29,20 +30,20 @@ def __main__():
 
 def print_field():
     # print column header
-    print "     1   2   3\n   +---+---+---+"
+    print("     1   2   3\n   +---+---+---+")
     for i in range(3):
         # print row header
-        print (i + 1), " | " + field[i][0] + " | " + field[i][1] + " | " + field[i][2] + " | "
-        print "   +---+---+---+"
+        print((i + 1), " | " + field[i][0] + " | " + field[i][1] + " | " + field[i][2] + " | ")
+        print("   +---+---+---+")
         
-    print ""
+    print("")
 
 def new_move(char):
     global fields
-    print "Next player is ", char
-    row = input("Row (1-3): ")
-    column = input("Column (1-3): ")
-    print ""
+    print("Next player is ", char)
+    row = raw_input("Row (1-3): ")
+    column = raw_input("Column (1-3): ")
+    print("")
     
     if (field[row-1][column-1] == LEER):
         field[row-1][column-1] = char
@@ -52,7 +53,7 @@ def new_move(char):
             return 0
     else:
         print_field()
-        print "This field is already taken."
+        print("This field is already taken.")
         
         if (char == 'X'):
             return O
@@ -63,7 +64,7 @@ def new_move(char):
     if (fields > 0):
         return char
     else:
-        print "All fields are taken."
+        print("All fields are taken.")
         return 0
 
 def is_winner():
@@ -76,7 +77,7 @@ def is_winner():
         or field[0][0] == player_one and field[1][1] == player_one and field[2][2] == player_one
         or field[0][2] == player_one and field[1][1] == player_one and field[2][0] == player_one
        ): 
-            print "Player has won"
+            print("Player {} has won", player_one)
             return True
     elif ( field[0][0] == player_two and field[0][1] == player_two and field[0][2] == player_two
         or field[1][0] == player_two and field[1][1] == player_two and field[1][2] == player_two
@@ -87,7 +88,7 @@ def is_winner():
         or field[0][0] == player_two and field[1][1] == player_two and field[2][2] == player_two
         or field[0][2] == player_two and field[1][1] == player_two and field[2][0] == player_two
        ): 
-            print "Player has won"
+            print("Player {} has won", player_two)
             return True
     return False
 
